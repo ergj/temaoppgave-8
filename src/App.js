63b0eb17c0348808ecdc34a33a44db6eac5447cb
ergja <erl1gjarum@gmail.com>
2021-05-11 17:09:12 +0200

    Lagt pÃ¥ litt infrastruktur

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
+
 function App() {
   return (
-    <>
-      <h1>Hallo verden!</h1>
-      <p>
-        Jeg er en veldig enkel React-app som du kan endre og leke med som du vil.
-      </p>
-    </>
+    <div>
+      
+      <a>
+        <p>Trykk her for å komme til den delte listen</p>
+      </a>
+      
+      <h1>Velkommen!</h1>
+      
+      <div>
+        <p>Dagens vær:</p>
+      </div>
+      
+    </div>
   )
 };
 
